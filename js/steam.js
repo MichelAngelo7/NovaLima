0e70732f6a1355d4e8c0047b259bcb09fe3a61f7
michelangelo7 <m_che_l@hotmail.com>
2020-09-18 20:25:42 -0300

    Game spam with the amount

--- js/steam.js
+++ js/steam.js
@@ -26,6 +26,20 @@ function showTop100(jsonObj){
   let top100 = jsonObj['0']
   console.log(top100)
   console.log(top100.length)
+  var lista = document.createElement('div')
+  
+  for(let i = 0; i < top100.length; i++){
+    for(let j = 0; j < 4; j++){
+      dados_lista = document.createElement('spam')
+      dados_lista.classList.add('corText')
+      lista.appendChild(dados_lista).innerHTML=`${top100[i][j]}<br>` 
+    }
+    lista.appendChild(dados_lista).innerHTML=`<br>`
+    body.appendChild(lista)
+  }
 
+  
 
 }
+
+body.appendChild(tableDiv)
