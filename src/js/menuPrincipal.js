52ff9385d02e3d4a3a52fe3b8cc452cbb9f2024e
michelangelo7 <m_che_l@hotmail.com>
2020-09-22 14:51:22 -0300

    Menu style

--- src/js/menuPrincipal.js
+++ src/js/menuPrincipal.js
@@ -3,26 +3,34 @@ export default class MenuPrincipal{
     //menu principal
     const nav = document.createElement('nav')
     nav.classList.add('menuPrincipal')
+    //div para conter os elementos isso ajuda no layout
+    const div = document.createElement('div')
+    nav.appendChild(div)
+
 
     //lista
     const lista = document.createElement('ul')
-    nav.appendChild(lista)
+    div.appendChild(lista)
     
 
     //cria o elemento home
     const home = document.createElement('li')
-    const homeTexto = document.createTextNode('Home')
-    home.appendChild(homeTexto)
+    home.innerHTML= '<a href="index.html">Home</a>'
   
     //cria o elemento veriadores
     const veriadores = document.createElement('li')
-    const veriadoresTexto = document.createTextNode('Veriadores')
-    veriadores.appendChild(veriadoresTexto)
+    veriadores.innerHTML =  '<a href="#">Veriadores</a>'
+    
+
+    //tramitacaoDeProjetos
+    const tramitacaoDeProjetos = document.createElement('li')
+    tramitacaoDeProjetos.innerHTML =
+    '<a href="#">Tramitação De Projetos</a>' 
     
     //adiciona os elementos
     lista.appendChild(home)
-    home.appendChild(veriadores)
-    
+    lista.appendChild(veriadores)
+    lista.appendChild(tramitacaoDeProjetos)
 
     return nav
 
